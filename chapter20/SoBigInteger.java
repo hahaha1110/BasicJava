9e92d1e877b4b1b421d347c2c06ad5ed9798d5c7
Hahyunji337 <gaulbaram111@naver.com>
2019-08-15 00:52:19 +0900

    0815 00:04

--- chapter20/SoBigInteger.java
+++ chapter20/SoBigInteger.java
@@ -8,8 +8,8 @@ import java.math.BigInteger;
 public class SoBigInteger {
 	public static void main(String[] args) {
 		// long형으로 표현 가능한 값의 크기 출력
-		System.out.println("최대 정수 : "+Long.MAX_VALUE);
-		System.out.println("최소 정수 : "+Long.MIN_VALUE);
+		System.out.println("최대 정수 : "+Long.MAX_VALUE); // Long형으로 표현할 수 있는 최댓값
+		System.out.println("최소 정수 : "+Long.MIN_VALUE); // Long형으로 표현할 수 있는 최솟값
 		System.out.println();
 		
 		// 매우 큰 수를 BingInteger 인스턴스로 표현
@@ -19,5 +19,25 @@ public class SoBigInteger {
 		// BigInteger 기반 덧셈
 		BigInteger r1 = big1.add(big2);
 		System.out.println("덧셈결과 : " + r1);
+		
+		// BigInteger 기반 뺄셈
+		BigInteger r2 = big1.subtract(big2);
+		System.out.println("뺄셈결과 : " + r2);
+		
+		// BigInteger 기반 곱셈
+		BigInteger r3 = big1.multiply(big2);
+		System.out.println("곱셈 결과 : "+r3);
+		
+		// BigInteger 기반 나눗셈의 몫
+		BigInteger r4 = big1.divide(big2);
+		System.out.println("나눗셈의 몫 : "+r4);
+		
+		// BigInteger 기반 나눗셈의 나머지
+		BigInteger r5 = big1.remainder(big2);
+		System.out.println("나눗셈의 나머지 : "+r5);
+		
+		// 인스턴스에 저장된 값을 int형 정수로 반환
+		int num = r1.intValueExact();
+		System.out.println("From BigInteger : "+num);
 	}
 }
