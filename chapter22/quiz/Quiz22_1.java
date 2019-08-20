01510803b3140e8ffa10de3e35d9fb39eebe5e02
Hahyunji337 <gaulbaram111@naver.com>
2019-08-21 00:59:30 +0900

    0821

---
+++ chapter22/quiz/Quiz22_1.java
@@ -0,0 +1,32 @@
+package chapter22.quiz;
+class Box<T>{
+	private T ob;
+	
+	public void set(T o) {
+		ob = o;
+	}
+	
+	public T get() {
+		return ob;
+	}
+}
+
+public class Quiz22_1 {
+	public static void addBox(Box<? extends Integer> b1, Box<? extends Integer> b2, Box<? super Integer> b3) {
+		b3.set(b1.get() + b2.get());
+	}
+	public static void main(String[] args) {
+		Box<Integer> box1 = new Box<>();
+		box1.set(24);
+		
+		Box<Integer> box2 = new Box<>();
+		box2.set(37);
+		
+		Box<Integer> result = new Box<>();
+		result.set(0);
+		
+		addBox(box1, box2, result);
+		
+		System.out.println(result.get());
+	}
+}
