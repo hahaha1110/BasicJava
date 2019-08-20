e4811dd6cd230ff7e0d855b781e1b63e96d2cd6b
Hahyunji337 <gaulbaram111@naver.com>
2019-08-20 20:02:00 +0900

    0820

---
+++ chapter22/WildcardUnboxer.java
@@ -0,0 +1,37 @@
+package chapter22;
+
+class Box<T>{
+	private T ob;
+	
+	public void set(T o) {
+		ob = o;
+	}
+	
+	public T get() {
+		return ob;
+	}
+	
+	@Override
+	public String toString(){
+		return ob.toString();
+	}
+}
+
+class Unboxer{
+	public static <T> T openBox(Box<T> box) {
+		return box.get();
+	}
+	
+	// 상자 안의 내용물을 출력하는 기능의 제네릭 메소드
+	public static <T> void peekBox(Box<T> box) {
+		System.out.println(box);
+	}
+}
+
+public class WildcardUnboxer {
+	public static void main(String[] args) {
+		Box<String> box = new Box<>();
+		box.set("남도현 귀여워");
+		Unboxer.peekBox(box);// 상자 안의 내용물을 확인해본다.
+	}
+}
