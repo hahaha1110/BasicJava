fe8f8d3d0938d42512cad952a5174ea07e76f12f
Hahyunji337 <gaulbaram111@naver.com>
2019-08-26 21:17:45 +0900

    0826

---
+++ chapter23/que/LinkedListQueue.java
@@ -0,0 +1,28 @@
+package chapter23.que;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LinkedListQueue {
+	public static void main(String[] args) {
+		// LinkedList 인스턴스 생성
+		Queue<String> que = new LinkedList<String>();
+		
+		que.offer("Box");
+		que.offer("Toy");
+		que.offer("Robot");
+		
+		// 무엇이 다음에 나올지 확인
+		System.out.println("next : " + que.peek());
+		
+		// 첫 번째, 두 번째 인스턴스 꺼내기
+		System.out.println(que.poll());
+		System.out.println(que.poll());
+		
+		// 무엇이 다음에 나올지 확인
+		System.out.println("next : " + que.peek());
+		
+		// 마지막 인스턴스 꺼내기
+		System.out.println(que.poll());
+	}
+}
