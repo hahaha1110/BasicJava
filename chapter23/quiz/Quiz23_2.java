e6858303b9f5dbe8aba2e962a178d3e0357f8fdc
Hahyunji337 <gaulbaram111@naver.com>
2019-08-26 11:27:34 +0900

    0826

--- chapter23/quiz/Quiz23_2.java
+++ chapter23/quiz/Quiz23_2.java
@@ -1,10 +1,19 @@
 package chapter23.quiz;
 
+import java.util.Comparator;
 import java.util.TreeSet;
+
+class IntComparator implements Comparator<Integer>{
+
+	@Override
+	public int compare(Integer o1, Integer o2) {
+		return o2 - o1;
+	}
+}
 
 public class Quiz23_2 {
 	public static void main(String[] args) {
-		TreeSet<Integer> tr = new TreeSet<>();
+		TreeSet<Integer> tr = new TreeSet<>(new IntComparator());
 		tr.add(30);
 		tr.add(10);
 		tr.add(20);
