7aa619c0048d3834690851f0e9fed20a94c8c23d
Hahyunji337 <gaulbaram111@naver.com>
2019-09-12 22:25:40 +0900

    0912

---
+++ chapter24/CarSortCollections.java
@@ -0,0 +1,39 @@
+package chapter24;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+class Car implements Comparable<Car>{
+	private int disp; // 배기량
+	
+	public Car(int d) {
+		disp = d;
+	}
+	
+	@Override
+	public String toString() {
+		return "cc: " + disp;
+	}
+
+	@Override
+	public int compareTo(Car o) {
+		return disp - o.disp;
+	}
+	
+}
+
+public class CarSortCollections {
+	public static void main(String[] args) {
+		List<Car> list = new ArrayList<Car>();
+		list.add(new Car(1200));
+		list.add(new Car(3000));
+		list.add(new Car(1800));
+		Collections.sort(list); // 정렬
+		
+		for (Iterator<Car> itr = list.iterator() ; itr.hasNext() ;) {
+			System.out.println(itr.next().toString() + "\n");
+		}
+	}
+}
--- chapter24/SortCollections.java
+++ chapter24/SortCollections.java
@@ -2,11 +2,28 @@ package chapter24;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 public class SortCollections {
 	public static void main(String[] args) {
 		List<String> list = Arrays.asList("남도현", "강민희", "구정모", "전정국");
 		list = new ArrayList<>(list);
+		
+		// 정렬 이전 출력
+		for(Iterator<String> itr = list.iterator();itr.hasNext();) {
+			System.out.print(itr.next() + "\t");
+		}
+		System.out.println();
+		
+		// 정렬
+		Collections.sort(list);
+		
+		// 정렬 이후 출력
+		for(Iterator<String> itr = list.iterator();itr.hasNext();) {
+			System.out.print(itr.next() + "\t");
+		}
+		System.out.println();
 	}
 }
