cc1cea2822b160fb43b4907e7e6e515dec26744d
Hahyunji337 <gaulbaram111@naver.com>
2019-09-22 18:05:13 +0900

    0922

---
+++ chapter34/CurrentThreadName.java
@@ -0,0 +1,11 @@
+package chapter34;
+
+public class CurrentThreadName {
+
+	public static void main(String[] args) {
+		Thread ct = Thread.currentThread();
+		String name = ct.getName(); // 쓰레드의 이름을 반환
+		System.out.println(name);
+	}
+
+}
---
+++ chapter34/MakeThreadDemo.java
@@ -0,0 +1,16 @@
+package chapter34;
+
+public class MakeThreadDemo {
+	public static void main(String[] args) {
+		Runnable task = () -> {// 쓰레드가 실행하게 할 내용
+			int n1 = 10;
+			int n2 = 20;
+			String name = Thread.currentThread().getName();
+			System.out.println(name + ":" + (n1 + n2));
+		};
+		
+		Thread t = new Thread(task);
+		t.start(); // 쓰레드의 생성 및 실행
+		System.out.println("End " + Thread.currentThread().getName());
+	}
+}
